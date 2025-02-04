21c19fa5ed2d0855f4770f93da7ddd5b113e9e18
Caroline Cerbelaud <caroline.cerbelaud@gmail.com>
2025-02-04 19:17:25 +0100

    add ended and abandoned status to booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,5 @@
 class Booking < ApplicationRecord
-  enum status: { pending: 0, accepted: 1, rejected: 2, canceled: 3 }
+  enum status: { pending: 0, accepted: 1, rejected: 2, canceled: 3, ended: 4, abandoned: 5 }
   belongs_to :mouette
   belongs_to :renter, class_name: 'User'
 
@@ -9,8 +9,8 @@ class Booking < ApplicationRecord
   validates :end_date, comparison: { greater_than_or_equal_to: :start_date }
   # status in a list of statuses["pending","accepted","rejected"]
   validates :status, inclusion: {
-        in: ["pending","accepted","rejected","canceled"],
-        message: 'Must select : ["pending","accepted","rejected","canceled"]'
+        in: ["pending","accepted","rejected","canceled", "ended", "abandoned"],
+        message: 'Must select : ["pending","accepted","rejected","canceled", "ended", "abandoned"]'
       }
 
   validates_uniqueness_of :mouette, scope: %i[start_date end_date renter]
