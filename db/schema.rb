f635bffd5c5dfb70ffc3189e0cf7873d7ff2b7eb
Caroline Cerbelaud <caroline.cerbelaud@gmail.com>
2025-02-06 17:50:27 +0100

    run AddAdminToUser migration and create Admin user

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_22_130111) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_06_151702) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_stat_statements"
   enable_extension "plpgsql"
 
   create_table "active_storage_attachments", force: :cascade do |t|
@@ -82,6 +83,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_22_130111) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
