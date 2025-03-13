891f3c9c2c1d52f04509375ca6993080094a4450
Caroline Cerbelaud <caroline.cerbelaud@gmail.com>
2025-03-13 14:18:42 +0100

    mise en place de la méthode du job de clean avec les conditions de status et de dates

--- app/jobs/bookings_daily_cleaning_job.rb
+++ app/jobs/bookings_daily_cleaning_job.rb
@@ -1,7 +1,35 @@
 class BookingsDailyCleaningJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    # Do something later
+  def perform
+    # bookings en pending (ni acceptés ni refusés)
+    pending_bookings = Booking.where(status:"pending")
+    pending_bookings.each do |booking|
+      # si la date de début est passée -> rejected
+      if booking.start_date < Date.today
+        booking.update(status:"rejected")
+      # si la date de fin est passée -> abandoned
+      elsif booking.end_date < Date.today
+        booking.update(status:"abandoned")
+      end
+    end
+
+    # bookings accepted
+    accepted_bookings = Booking.where(status:"accepted")
+    accepted_bookings.each do |booking|
+      # si la date de fin est passée -> ended
+      if booking.end_date < Date.today
+        booking.update(status:"ended")
+      end
+    end
+
+    # bookings rejected
+    rejected_bookings = Booking.where(status:"rejected")
+    rejected_bookings.each do |booking|
+      # si la date de fin est passée -> abandoned	if booking.end_date < Date.today
+      if booking.end_date < Date.today
+        booking.update(status:"ended")
+      end
+    end
   end
 end
