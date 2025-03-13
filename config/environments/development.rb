32b853b42a2914319b4f67394786f83ae7ad3ebb
Caroline Cerbelaud <caroline.cerbelaud@gmail.com>
2025-03-13 16:06:24 +0100

    ajout de logs dans le job et gestion de l'écriture des logs dans le fichier de log

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,6 +17,10 @@ Rails.application.configure do
   # Enable server timing
   config.server_timing = true
 
+  # Allows logs to be written in log file
+  config.logger = ActiveSupport::Logger.new("log/development.log")
+  config.log_level = :debug
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join("tmp/caching-dev.txt").exist?
